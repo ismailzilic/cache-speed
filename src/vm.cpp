89ad120ff4988190769036d531b2ceb022ca8f14
Ismail Zilic <ismailzilic@gmail.com>
2026-07-13 21:38:17 +0200

    update: win api for vm_get_page_size

---
+++ src/vm.cpp
@@ -0,0 +1,31 @@
+#include "vm.h"
+
+#if defined(_WIN32)
+
+#include <windows.h>
+
+#elif defined(__linux__)
+
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE
+#endif
+#include <unistd.h>
+
+#endif
+
+uint32_t vm_get_page_size(void)
+{
+#if defined(_WIN32)
+	SYSTEM_INFO sysinfo;
+	GetSystemInfo(&sysinfo);
+	return sysinfo.dwPageSize;
+#elif defined(__linux__)
+	return static_cast<uint32_t>(sysconf(_SC_PAGESIZE));
+
+#else
+
+#include <stdexcept>
+	throw std::runtime_error("unsupported platform.\n");
+
+#endif
+}
---
+++ src/vm.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cstdint>
+
+uint32_t vm_get_page_size(void);
+
